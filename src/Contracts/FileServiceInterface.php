dab6b0e8878f8786b39a34d2df121759cc46227e
amir <amirjoun2@gmail.com>
2025-02-16 01:19:32 +0400

    Added strict types

--- src/Contracts/FileServiceInterface.php
+++ src/Contracts/FileServiceInterface.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SimpleEnv\Contracts;
 
 interface FileServiceInterface
 {
     public function checkFileExist(string $filePath): bool;
-    
+
     public function getFileContent(string $filePath);
 }
--- src/Env.php
+++ src/Env.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SimpleEnv;
 
 use SimpleEnv\Enums\File;
