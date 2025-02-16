38f67af56943308fcf01609803e649bc34e7242d
amir <amirjoun2@gmail.com>
2025-02-16 22:40:35 +0400

    Refactored and added methods

--- src/Services/FileService.php
+++ src/Services/FileService.php
@@ -2,28 +2,38 @@
 
 declare(strict_types=1);
 
-namespace SimpleEnv\Services;
+namespace EnvLoader\Services;
 
-use SimpleEnv\Contracts\FileServiceInterface;
+use EnvLoader\Contracts\Services\FileServiceInterface;
 
 class FileService implements FileServiceInterface
 {
     public function checkFileExist(string $filePath): bool
     {
         if (!file_exists($filePath)) {
-            return false;//TODO Exception
+            throw new \RuntimeException("Env file not found at: $filePath");
         }
+
         return true;
     }
 
-    public function getFileContent(string $filePath)
+    public function getFileContent(string $filePath): ?string
     {
         $content = file_get_contents($filePath, false);
 
         if (strlen($content) === 0) {
-            return false;//TODO Exception 
+            return null;
         }
 
         return $content;
+    }
+
+    public function validatedContent(string $path, string $fileName): ?string
+    {
+        $filePath = "$path/$fileName";
+
+        $this->checkFileExist($filePath);
+
+        return $this->getFileContent($filePath);
     }
 }
