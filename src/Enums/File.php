66cfa57474d72c7f38af1204da053e16c7203133
amir <amirjoun2@gmail.com>
2025-02-16 22:37:02 +0400

    Added a variable

--- src/Enums/File.php
+++ src/Enums/File.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace SimpleEnv\Enums;
+namespace EnvLoader\Enums;
 
 enum File : string
 {
     case DEFAULTNAME = '.env';
-
+    case DEFAULTCONFIGFILE = 'env';
 }
