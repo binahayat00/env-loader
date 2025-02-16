c2f2ea3bbc3fc3bd02a92002e9614b0b138e9a6c
amir <amirjoun2@gmail.com>
2025-02-16 22:36:21 +0400

    Refactored Env.php

--- src/Env.php
+++ src/Env.php
@@ -2,78 +2,43 @@
 
 declare(strict_types=1);
 
-namespace SimpleEnv;
+namespace EnvLoader;
 
-use SimpleEnv\Enums\File;
-use SimpleEnv\Contracts\FileServiceInterface;
+use EnvLoader\Enums\File;
+use EnvLoader\Services\ConfigLoader;
+use EnvLoader\Contracts\Services\FileServiceInterface;
+use EnvLoader\Contracts\Services\LineServiceInterface;
+use EnvLoader\Services\Config;
 
 class Env
 {
+    protected ?Config $config;
+
     public function __construct(
-        public FileServiceInterface $fileService
+        protected FileServiceInterface $fileService,
+        protected LineServiceInterface $lineService
     ) {
     }
 
-    public function get($path, $fileName = File::DEFAULTNAME->value): array
+    public function load(string $path, ?string $fileName = null, ?string $configPath = null,?string $configFileName = File::DEFAULTCONFIGFILE->value): array
     {
-        $filePath = "$path/$fileName";
-
-        if (!$this->fileService->checkFileExist($filePath)) {
-            return [];
-        }
-
-        $content = $this->fileService->getFileContent($filePath);
-
-        if (!$content) {
+        $this->config = $this->setConfigs($configFileName, $configPath);
+        $content = $this->fileService->validatedContent($path, $fileName ?? $this->config->get('file.name.default'));
+        if ($content === null) {
             return [];
         }
 
         $convertedContent = mb_convert_encoding($content, 'UTF-8');
-        $splitContent = (array) preg_split("/(\r\n|\n|\r)/", $convertedContent);
 
-        if (\preg_last_error() !== \PREG_NO_ERROR) {
-            preg_last_error_msg();
-            return [];
-        }
+        $lines = $this->lineService->splitToLines($convertedContent);
 
-        $lines = [];
-        $buffer = [];
-        foreach ($splitContent as $line) {
-            if (strpos($line, '="') !== false) {
-                //looksLikeMultilineStart
-                if ($line !== '"') {
-                    $line = str_replace('\\\\', '', $line);
-                    $count = (int) preg_match_all('/(?=([^\\\\]"))/', $line);
-
-                    if ($count > 1) {
-                        array_push($buffer, $line);
-                        $line = \implode("\n", $buffer);
-                    }
-                }
-            }
-
-            //isCommentOrWhitespace
-            $line = trim($line);
-            if ($line === '' || (isset($line[0]) && $line[0] === '#')) {
-                return [];
-            }
-
-            [$key, $value] = array_map('trim', \explode('=', $line, 2));
-            $value = preg_replace(
-                '/^"(.*)"$/',
-                '$1',
-                trim($value),
-            );
-
-            $lines[$key] = $value;
-            $this->setToEnv($key, $value);
-        }
-
-        return $lines;
+        return $this->lineService->handleLines($lines);
     }
 
-    public function setToEnv(string $key, string $value): void
+    private function setConfigs(string $configFileName, ?string $configPath)
     {
-        $_ENV[$key] = $value;
+        $config = ConfigLoader::loadStatic($configFileName, $configPath);
+        return $this->config = new Config($config);
+
     }
 }
